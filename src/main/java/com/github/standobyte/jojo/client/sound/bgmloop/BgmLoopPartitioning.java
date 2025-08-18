31823fdcc21f35fec0fe76a0d6858e109cf6e39e
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-08-18 14:28:34 +0300

    parsing fl studio note subdivisions

--- src/main/java/com/github/standobyte/jojo/client/sound/bgmloop/BgmLoopPartitioning.java
+++ src/main/java/com/github/standobyte/jojo/client/sound/bgmloop/BgmLoopPartitioning.java
@@ -3,10 +3,10 @@ package com.github.standobyte.jojo.client.sound.bgmloop;
 import java.util.EnumMap;
 import java.util.Map;
 
-import com.github.standobyte.jojo.util.JSONUtil;
 import com.github.standobyte.jojo.util.java.OptionalFloat;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 import net.minecraft.resources.FileToIdConverter;
 
@@ -25,10 +25,10 @@ public record BgmLoopPartitioning(Map<BgmPart, Partition> partition) {
 		JsonObject jsonObj = json.getAsJsonObject();
 		return fromTimestamps(
 				jsonObj.get("bpm").getAsFloat(), 
-				JSONUtil.getFloatOr("intro", jsonObj, 0),
-				JSONUtil.getFloatOr("loopStart", jsonObj, 0),
-				JSONUtil.getFloatOptional("loopBack", jsonObj),
-				JSONUtil.getFloatOptional("outro", jsonObj));
+				getFloatOr("intro", jsonObj, 0, BgmLoopPartitioning::parseNote),
+				getFloatOr("loopStart", jsonObj, 0, BgmLoopPartitioning::parseNote),
+				getFloatOptional("loopBack", jsonObj, BgmLoopPartitioning::parseNote),
+				getFloatOptional("outro", jsonObj, BgmLoopPartitioning::parseNote));
 	}
 	
 	public static BgmLoopPartitioning fromTimestamps(float bpm, float intro, float loopStart, OptionalFloat loopBack, OptionalFloat outro) {
@@ -57,4 +57,49 @@ public record BgmLoopPartitioning(Map<BgmPart, Partition> partition) {
 	public static float toSecs(float note, float bpm) {
 		return note * 240 /*4 beats * 60 seconds*/ / bpm;
 	}
+	
+	// parsing stuff
+	
+	public static float parseNote(JsonElement json) {
+		JsonPrimitive element = json.getAsJsonPrimitive();
+		if (element.isNumber()) {
+			return json.getAsFloat();
+		}
+		else if (element.isString()) {
+			String string = element.getAsString();
+			String[] split = string.split(":");
+			if (split.length > 3) {
+				throw new IllegalArgumentException("Failed to parse note (too many note subdivisions)");
+			}
+			int[] values = new int[] {
+				Integer.parseInt(split[0]),
+				split.length > 1 ? Integer.parseInt(split[1]) : 0,
+				split.length > 2 ? Integer.parseInt(split[2]) : 0,
+			};
+			return values[0] + (float) values[1] / 16f + (float) values[2] / 24f;
+		}
+		throw new IllegalArgumentException("Failed to parse note (not a number or string)");
+	}
+
+	static float getFloatOr(String key, JsonObject json, float defaultValue, NoteParse parse) {
+		JsonElement jsonelement = json.get(key);
+		if (jsonelement != null) {
+			return jsonelement.isJsonNull() ? defaultValue : parse.parse(jsonelement);
+		} else {
+			return defaultValue;
+		}
+	}
+
+	static OptionalFloat getFloatOptional(String key, JsonObject json, NoteParse parse) {
+		JsonElement jsonelement = json.get(key);
+		if (jsonelement != null) {
+			return jsonelement.isJsonNull() ? OptionalFloat.empty() : OptionalFloat.of(parse.parse(jsonelement));
+		} else {
+			return OptionalFloat.empty();
+		}
+	}
+	
+	@FunctionalInterface
+	static interface NoteParse { float parse(JsonElement json); }
+	
 }
