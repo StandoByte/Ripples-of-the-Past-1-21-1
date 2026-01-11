38bdd1bf0e9e416da55ba4ac23ac2a49cf8c3b28
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-01-12 02:04:30 +0300

    this shit is so stupid i have to do this now before i forget

---
+++ src/main/java/com/github/standobyte/jojo/jojoimpl/vampirism/abilities/VampFreezeAbility.java
@@ -0,0 +1,34 @@
+package com.github.standobyte.jojo.jojoimpl.vampirism.abilities;
+
+import com.github.standobyte.jojo.util.damage.DamageUtil;
+
+import net.minecraft.core.Holder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+
+public class VampFreezeAbility {
+
+	public static DamageSource freezeDamageSource(Entity vampireEntity) {
+		Holder<DamageType> type = DamageUtil.type(vampireEntity.level(), DamageTypes.FREEZE);
+		return new DamageSource(type, vampireEntity, vampireEntity, null) {
+
+			@Override
+			public Component getLocalizedDeathMessage(LivingEntity deadEntity) {
+				Entity causingEntity = this.getEntity();
+				Entity directEntity = this.getDirectEntity();
+				String tlKey = "death.attack." + this.type().msgId();
+				if (causingEntity == null && directEntity == null) {
+					return Component.translatable(tlKey, deadEntity.getDisplayName());
+				} else {
+					Entity killer = causingEntity == null ? directEntity : causingEntity;
+					Component killerName = killer.getDisplayName();
+					return Component.translatable(tlKey + ".player", deadEntity.getDisplayName(), killerName);
+				}
+			}
+		};
+	}
+}
