bfe698531fef3bd3152199d675e1d058ab3e4117
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-01-14 14:08:10 +0300

    Fixed block breaking with dropBlock false not remembering the block...
    if it leads to another block being broken (e.g. crops or grass breaking
    after a block underneath is being broken)

--- src/main/java/com/github/standobyte/jojo/mixin/stand/crazyd/LevelDestroyBlockMixin.java
+++ src/main/java/com/github/standobyte/jojo/mixin/stand/crazyd/LevelDestroyBlockMixin.java
@@ -1,10 +1,13 @@
 package com.github.standobyte.jojo.mixin.stand.crazyd;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.annotation.Nullable;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,8 +25,7 @@ import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(Level.class)
 public abstract class LevelDestroyBlockMixin {
-	private BlockState jojo_ripples$oldBlockState;
-	@Nullable private BlockEntity jojo_ripples$oldBlockEntity;
+	private Map<BlockPos, Pair<BlockState, Optional<BlockEntity>>> jojo_ripples$oldBlockTmp = new HashMap<>();
 
 	@Shadow public abstract BlockEntity getBlockEntity(BlockPos pos);
 
@@ -33,36 +35,29 @@ public abstract class LevelDestroyBlockMixin {
 					+ "getFluidState("
 					+ "Lnet/minecraft/core/BlockPos;"
 					+ ")Lnet/minecraft/world/level/material/FluidState;"),
-			require = 1,
 			locals = LocalCapture.CAPTURE_FAILSOFT)
 	public void jojo_ripples$rememberOldBlock(BlockPos pos, boolean dropBlock, @Nullable Entity entity, int recursionLeft, CallbackInfoReturnable<Boolean> ci, 
 			BlockState blockstate) {
-		if (!dropBlock) {
-			jojo_ripples$oldBlockState = blockstate;
-			if (jojo_ripples$oldBlockState != null) {
-				jojo_ripples$oldBlockEntity = jojo_ripples$oldBlockState.hasBlockEntity() ? this.getBlockEntity(pos) : null;
-			}
+		if (!dropBlock && blockstate != null) {
+			BlockEntity blockEntity = blockstate.hasBlockEntity() ? this.getBlockEntity(pos) : null;
+			jojo_ripples$oldBlockTmp.put(pos.immutable(), Pair.of(blockstate, Optional.ofNullable(blockEntity)));
 		}
 	}
 
 	@Inject(method = "destroyBlock", at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/world/level/Level;"
-					+ "gameEvent("
-					+ "Lnet/minecraft/core/Holder;"
-					+ "Lnet/minecraft/core/BlockPos;"
-					+ "Lnet/minecraft/world/level/gameevent/GameEvent$Context;"
-					+ ")V"),
+			value = "RETURN",
+			ordinal = 1),
 			require = 1)
 	public void jojo_ripples$onBlockDestroyNoDrops(BlockPos pos, boolean dropBlock, @Nullable Entity entity, int recursionLeft, CallbackInfoReturnable<Boolean> ci) {
-		if (!dropBlock && jojo_ripples$oldBlockState != null) {
-			Level level = (Level) (Object) this;
-			CrazyDRestoreTerrainAbility.rememberBrokenBlock(level, pos, 
-					jojo_ripples$oldBlockState, 
-					Optional.ofNullable(jojo_ripples$oldBlockEntity), 
-					Collections.emptyList());
+		if (!dropBlock) {
+			Pair<BlockState, Optional<BlockEntity>> oldBlock = jojo_ripples$oldBlockTmp.remove(pos);
+			if (ci.getReturnValueZ() && oldBlock != null) {
+				Level level = (Level) (Object) this;
+				CrazyDRestoreTerrainAbility.rememberBrokenBlock(level, pos, 
+						oldBlock.getLeft(), 
+						oldBlock.getRight(), 
+						Collections.emptyList());
+			}
 		}
-		jojo_ripples$oldBlockState = null;
-		jojo_ripples$oldBlockEntity = null;
 	}
 }
