05bebf35723989121bb6185db5cf14470d4108de
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-02-26 13:54:22 +0300

    whatever

--- src/main/java/com/github/standobyte/jojoimpl/stands/_entitybase/StandEntityBarrageAbility.java
+++ src/main/java/com/github/standobyte/jojoimpl/stands/_entitybase/StandEntityBarrageAbility.java
@@ -35,7 +35,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SoundType;
@@ -115,7 +114,6 @@ public class StandEntityBarrageAbility extends StandEntityAbility {
 								() -> this.isOver() || this.phase != ActionPhase.PERFORM));
 					}
 				}
-				tossStandHeldItems(EquipmentSlot.OFFHAND, EquipmentSlot.MAINHAND);
 			}
 		}
 		
