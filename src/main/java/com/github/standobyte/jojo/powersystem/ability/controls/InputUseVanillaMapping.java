2a638b08546638a7c82f6bb2ef4c0543058f74b4
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-02-27 14:42:00 +0300

    makes a bit more sense

--- src/main/java/com/github/standobyte/jojo/powersystem/ability/controls/InputUseVanillaMapping.java
+++ src/main/java/com/github/standobyte/jojo/powersystem/ability/controls/InputUseVanillaMapping.java
@@ -1,5 +1,9 @@
 package com.github.standobyte.jojo.powersystem.ability.controls;
 
+import javax.annotation.Nullable;
+
+import com.github.standobyte.jojo.util.reflection.ClientReflection;
+
 import net.minecraft.client.KeyMapping;
 
 public class InputUseVanillaMapping implements InputBindTemplate {
@@ -12,4 +16,9 @@ public class InputUseVanillaMapping implements InputBindTemplate {
 	public InputUseVanillaMapping(String keyName) {
 		this.keyMappingName = keyName;
 	}
+    
+    @Nullable
+    public KeyMapping toClientKeybind() {
+    	return ClientReflection.getKeyMappingMapByName().get(this.keyMappingName);
+    }
 }
