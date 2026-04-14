167cf16a7ac16a248dbae1172da6ba8347327555
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-04-14 23:16:05 +0300

    we don't need this actually

--- src/main/java/com/github/standobyte/jojo/mechanics/clothes/container/PlayerClothesMenu.java
+++ src/main/java/com/github/standobyte/jojo/mechanics/clothes/container/PlayerClothesMenu.java
@@ -23,6 +23,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ArmorSlot;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
