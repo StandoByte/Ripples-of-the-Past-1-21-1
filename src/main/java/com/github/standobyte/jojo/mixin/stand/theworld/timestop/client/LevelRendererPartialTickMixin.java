b79c4e9d0d5479b10ca0cf81143ca4d8f3735fba
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-04-29 22:02:30 +0300

    more visuals pausing mixins from 1.16.5

--- src/main/java/com/github/standobyte/jojo/mixin/stand/theworld/timestop/client/LevelRendererPartialTickMixin.java
+++ src/main/java/com/github/standobyte/jojo/mixin/stand/theworld/timestop/client/LevelRendererPartialTickMixin.java
@@ -2,7 +2,9 @@ package com.github.standobyte.jojo.mixin.stand.theworld.timestop.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.github.standobyte.jojoimpl.stands.theworld.timestop.client.TimeStopClientState;
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -13,6 +15,29 @@ import net.minecraft.world.entity.Entity;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererPartialTickMixin {
+
+	@ModifyVariable(method = "renderSnowAndRain", at = @At("HEAD"), argsOnly = true, ordinal = 0)
+	private float weatherChangePartialTick(float partialTick) {
+		if (TimeStopClientState.isTimeStopped) {
+			return 1;
+		}
+		return partialTick;
+	}
+
+	@ModifyVariable(method = "renderClouds", at = @At("HEAD"), argsOnly = true, ordinal = 0)
+	private float cloudsChangePartialTick(float partialTick) {
+		if (TimeStopClientState.isTimeStopped) {
+			return 1;
+		}
+		return partialTick;
+	}
+
+	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+	public void cancelTickCountIncrement(CallbackInfo ci) {
+		if (TimeStopClientState.isTimeStopped) {
+			ci.cancel();
+		}
+	}
 
 	@ModifyVariable(method = "renderEntity", at = @At("HEAD"), argsOnly = true, ordinal = 0)
 	public float partialTick(float partialTick, 
