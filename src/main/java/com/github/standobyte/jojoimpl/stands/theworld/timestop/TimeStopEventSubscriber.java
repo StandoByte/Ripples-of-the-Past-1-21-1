48ce03997f3079371cf296e0818fc4746f703c4e
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-04-30 21:51:34 +0300

    decrease iticks and item pickup delay

--- src/main/java/com/github/standobyte/jojoimpl/stands/theworld/timestop/TimeStopEventSubscriber.java
+++ src/main/java/com/github/standobyte/jojoimpl/stands/theworld/timestop/TimeStopEventSubscriber.java
@@ -3,6 +3,8 @@ package com.github.standobyte.jojoimpl.stands.theworld.timestop;
 import com.github.standobyte.jojo.core.JojoMod;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -18,6 +20,26 @@ public class TimeStopEventSubscriber {
 		if (isStoppedInTime) {
 			entity.tickCount--;
 			event.setCanceled(true);
+			tickInTimeStopAnyway(entity);
 		}
 	}
+	
+	public static void tickInTimeStopAnyway(Entity entity) {
+		if (!entity.level().isClientSide()) {
+			if (entity instanceof LivingEntity living) {
+				if (entity.invulnerableTime > 0) {
+					entity.invulnerableTime--;
+				}
+			}
+			else if (entity instanceof ItemEntity itemEntity) {
+				if (itemEntity.pickupDelay > 0 && itemEntity.pickupDelay != 32767) {
+					--itemEntity.pickupDelay;
+				}
+			}
+		}
+		if (entity instanceof TickEntityInTimeStop e) {
+			e.tickInTimeStop();
+		}
+	}
+	
 }
