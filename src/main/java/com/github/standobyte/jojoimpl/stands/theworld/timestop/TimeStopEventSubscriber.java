756baf2b27b29ff9af5d1731649080b7d4108b41
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-04-30 23:25:47 +0300

    later

--- src/main/java/com/github/standobyte/jojoimpl/stands/theworld/timestop/TimeStopEventSubscriber.java
+++ src/main/java/com/github/standobyte/jojoimpl/stands/theworld/timestop/TimeStopEventSubscriber.java
@@ -10,6 +10,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
 
+// TODO (time stop) arrow 5 ticks inertia
 @EventBusSubscriber(modid = JojoMod.MOD_ID)
 public class TimeStopEventSubscriber {
 
