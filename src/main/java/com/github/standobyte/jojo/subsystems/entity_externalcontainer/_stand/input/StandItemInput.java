d96fa2502325db60a4a92a7e36355e22b3582c20
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-06-04 18:31:50 +0300

    Fixed Q and Ctrl+Q being reversed for Stands

--- src/main/java/com/github/standobyte/jojo/subsystems/entity_externalcontainer/_stand/input/StandItemInput.java
+++ src/main/java/com/github/standobyte/jojo/subsystems/entity_externalcontainer/_stand/input/StandItemInput.java
@@ -21,21 +21,10 @@ public class StandItemInput {
 
 	public static void handlePacket(Action action, StandEntity standEntity, LivingEntity user) {
 		switch (action) {
-			case DROP -> {
-				if (!standEntity.getMainHandItem().isEmpty()) {
-					standEntity.tossItem(InteractionHand.MAIN_HAND, false);
-				}
-				else {
-					standEntity.tossItem(InteractionHand.OFF_HAND, false);
-				}
-			}
-			case DROP_FULL_STACK -> {
-				if (!standEntity.getMainHandItem().isEmpty()) {
-					standEntity.tossItem(InteractionHand.MAIN_HAND, true);
-				}
-				else {
-					standEntity.tossItem(InteractionHand.OFF_HAND, true);
-				}
+			case DROP, DROP_FULL_STACK -> {
+				boolean singleItem = action != Action.DROP_FULL_STACK;
+				InteractionHand hand = !standEntity.getMainHandItem().isEmpty() ? InteractionHand.MAIN_HAND : InteractionHand.OFF_HAND;
+				standEntity.tossItem(hand, singleItem);
 			}
 			case SWAP_HANDS -> {
 				ItemStack lItem = standEntity.getOffhandItem();
