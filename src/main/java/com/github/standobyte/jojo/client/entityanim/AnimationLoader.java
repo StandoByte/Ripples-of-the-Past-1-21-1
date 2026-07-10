02c6b0572fc26a0f6a3fe4cd17ed946558f113f1
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-07-10 19:57:54 +0300

    updated rig

--- src/main/java/com/github/standobyte/jojo/client/entityanim/AnimationLoader.java
+++ src/main/java/com/github/standobyte/jojo/client/entityanim/AnimationLoader.java
@@ -13,6 +13,8 @@ import com.github.standobyte.jojo.client.entityanim.molang.KeyframesMolangEngine
 import com.github.standobyte.jojo.core.JojoMod;
 import com.github.standobyte.jojo.util.functions.JSONUtil;
 import com.github.standobyte.jojo.util.functions.StringUtil;
+import com.github.standobyte.v1_21_4_stuff.missingmethods.Zone;
+import com.github.standobyte.v1_21_4_stuff.missingmethods._ProfilerFiller;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
@@ -21,10 +23,6 @@ import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
-
-import com.github.standobyte.v1_21_4_stuff.missingmethods.Zone;
-import com.github.standobyte.v1_21_4_stuff.missingmethods._ProfilerFiller;
-
 import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
 
 public class AnimationLoader extends SimplePreparableReloadListener<Map<ResourceLocation, AnimationSet.Builder>> {
@@ -117,7 +115,9 @@ public class AnimationLoader extends SimplePreparableReloadListener<Map<Resource
 	@Override
 	protected void apply(Map<ResourceLocation, AnimationSet.Builder> skinsRead, ResourceManager resourceManager, ProfilerFiller profiler) {
 		this.anims.clear();
-		skinsRead.forEach((key, animBuilder) -> this.anims.put(key, animBuilder.build()));
+		skinsRead.forEach((key, animBuilder) -> {
+			this.anims.put(key, animBuilder.build());
+		});
 		JojoMod.getLogger().info("Loaded {} entity animation files", this.anims.size());
 	}
 	
